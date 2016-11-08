4dfd4543d37e3f72a9de7a1dc467d166c954be58
Zhenyu Wu <adamwu@nec-labs.com>
2016-11-08 18:42:22 -0500

    Ensure compatibility with Microsoft Edge

--- src/datetimeranger.js
+++ src/datetimeranger.js
@@ -1072,13 +1072,13 @@
 								'class': 'input-wrapper'
 							}, tagGen('input', {
 									'class': 'date',
-									'type': 'date',
+									'type': /Edge/.test(navigator.userAgent) ? 'text' : 'date',
 									'min': '2000-01-01',
 									'max': '3000-01-01'
 								})
 								 + tagGen('input', {
 									'class': 'time',
-									'type': 'time'
+									'type': /Edge/.test(navigator.userAgent) ? 'text' : 'time'
 								})))
 						 + tagGen('div', {
 							'class': 'pickers'
@@ -1091,13 +1091,13 @@
 								'class': 'input-wrapper'
 							}, tagGen('input', {
 									'class': 'date',
-									'type': 'date',
+									'type': /Edge/.test(navigator.userAgent) ? 'text' : 'date',
 									'min': '2000-01-01',
 									'max': '3000-01-01'
 								})
 								 + tagGen('input', {
 									'class': 'time',
-									'type': 'time'
+									'type': /Edge/.test(navigator.userAgent) ?' text ':'time'
 								})))
 						 + tagGen('div', {
 							'class': 'pickers'
